0f17eafee19f71f231910f1003d446b5a8fd825f
MaxwelSilvas <maxwel101@yahoo.com>
2024-07-04 19:05:19 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/MaxwelSilvas.png" alt="" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/MaxwelSilvas.png" alt="" />
-    <Titulo fontSize={20}>Maxwel Silva</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Maxwel Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        MaxwelSilvas
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor FullStack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
